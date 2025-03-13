a46786e59d02e77c41762b52bcb4c74cc83abde2
Waldo Ramos <waldreek@gmail.com>
2025-03-12 21:43:13 -0300

    Demo v1.0

---
+++ README.md
@@ -0,0 +1,4 @@
+# my-shop
+Este sitio está en desarrollo, recibe actualizaciones constantemente y se puede considerar su inestabilidad, por lo tanto las actualizaciones lo van a estar mejorando.
+El sitio será liberado o puesto en publico en cuanto este bastante estable y renderizado en los proximos meses.
+Mientras tanto solo los VIP pueden acceder a este sitio.
---
+++ script.js
@@ -0,0 +1,92 @@
+const openButton = document.getElementById('open-sidebar-button')
+const navbar = document.getElementById('navbar')
+
+const media = window.matchMedia("(width < 700px)")
+
+media.addEventListener('change', (e) => updateNavbar(e))
+
+function updateNavbar(e){
+  const isMobile = e.matches
+  console.log(isMobile)
+  if(isMobile){
+    navbar.setAttribute('inert', '')
+  }
+  else{
+    // desktop device
+    navbar.removeAttribute('inert')
+  }
+}
+
+function openSidebar(){
+  navbar.classList.add('show')
+  openButton.setAttribute('aria-expanded', 'true')
+  navbar.removeAttribute('inert')
+}
+
+function closeSidebar(){
+  navbar.classList.remove('show')
+  openButton.setAttribute('aria-expanded', 'false')
+  navbar.setAttribute('inert', '')
+}
+
+document.addEventListener( 'DOMContentLoaded', function () {
+  new Splide( '#image-carousel' ).mount();
+} );
+// For Bookmark Links
+// const navLinks = document.querySelectorAll('nav a')
+// navLinks.forEach(link => {
+//   link.addEventListener('click', () => {
+//     closeSidebar()
+//   })
+// })
+
+updateNavbar(media)
+
+const btnLeft = document.querySelector(".btn-left"),
+      btnRight = document.querySelector(".btn-right"),
+      slider = document.querySelector("#slider"),
+      sliderSection = document.querySelectorAll(".slider-section");
+
+
+btnLeft.addEventListener("click", e => moveToLeft())
+btnRight.addEventListener("click", e => moveToRight())
+
+setInterval(() => {
+    moveToRight()
+}, 4000);
+
+
+let operacion = 0,
+    counter = 0,
+    widthImg = 100 / sliderSection.length;
+
+function moveToRight() {
+    if (counter >= sliderSection.length-1) {
+        counter = 0;
+        operacion = 0;
+        slider.style.transform = `translate(-${operacion}%)`;
+        slider.style.transition = "none";
+        return;
+    } 
+    counter++;
+    operacion = operacion + widthImg;
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "all ease .6s"
+    
+}  
+
+function moveToLeft() {
+    counter--;
+    if (counter < 0 ) {
+        counter = sliderSection.length-1;
+        operacion = widthImg * (sliderSection.length-1)
+        slider.style.transform = `translate(-${operacion}%)`;
+        slider.style.transition = "none";
+        return;
+    } 
+    operacion = operacion - widthImg;
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "all ease .6s"
+    
+    
+}   
